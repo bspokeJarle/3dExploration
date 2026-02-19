70a3160a05f521dcfd5b963f77080b1b0d7aa6c1
bspokeJarle <46017737+bspokeJarle@users.noreply.github.com>
2026-02-19 15:10:58 +0100

    More performance upgrades, refactors to create TriangleMeshes faster.

--- BenchmarkSuite1/Program.cs
+++ BenchmarkSuite1/Program.cs
@@ -6,7 +6,7 @@ namespace BenchmarkSuite1
     {
         static void Main(string[] args)
         {
-            var _ = BenchmarkRunner.Run(typeof(Program).Assembly);
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
 }
